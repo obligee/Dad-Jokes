dcb417f1affccb0270116dbcdc87a7edfac33cc6
John Dough <simoncm4@vcu.edu>
2025-10-04 15:40:57 -0400

    Made function async

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const jokeEl = document.getElementById('joke');
 const jokeBtn = document.getElementById('jokeBtn');
 
-function generateJoke() {
+async function generateJoke() {
    const config = {
      headers: {
        Accept: 'application/json',
      },
    };
 
-   fetch('https://icanhazdadjoke.com', config)
+   await fetch('https://icanhazdadjoke.com', config)
      .then((res) => res.json())
      .then((data) => {
-       jokeEl.innerHTML = data.joke;
+       jokeEl.textContent = data.joke;
      });
  }
 
 generateJoke();
+
+// Button event listener
+jokeBtn.addEventListener('click', generateJoke);
